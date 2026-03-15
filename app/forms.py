dff372698be44e7949aff963983e9fafa3cdec59
maximeqa <wompitywompwomp123@gmail.com>
2026-03-15 03:06:01 +0000

    HTML fixes

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,6 @@ from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2,max=150)])
     password = PasswordField('Password', validators=[DataRequired()])
-    admin = BooleanField('Login as Admin')
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
